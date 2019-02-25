58d08cb7ec4ea4f4383cb25bab0425e1ab4f6c14
Ayush Khandelwal <ayushkhandelwal93@gmail.com>
2019-02-25 23:48:18 +0530

    made 2 fns running in thread without sync and no mutex used

--- app3/app3.cpp
+++ app3/app3.cpp
@@ -1,8 +1,46 @@
 #include<iostream>
 #include<thread>
+#include<mutex>
 using namespace std;
+
+mutex m;
+void f1()
+{
+    cout<<"f1"<<endl;
+    cout<<"f1"<<endl;
+}
+void f2()
+{
+    cout<<"f2"<<endl;
+    cout<<"f2"<<endl;
+}
 int main()
 {
+
+    thread t1(f1);
+
+    f2();
+
+    t1.join();
+
+
+    /*
+    No synchronization achieved yet
+    2 diff results in 2 executions
+    RESULT-1
+    f2
+    f2
+    f1
+    f1
+    RESULT-2
+    f2f1
+    f2
+
+    f1
+    */
+
+
+    /*
     //// Main Thread creates t1 object and calls its ctor.
 	//// In the ctro secondary thread is created.
 	//// Main thread comes out of the ctor and goes to next instruction
@@ -16,4 +54,5 @@ int main()
     {
         cout<<"W2"<<endl;
     }
+    */
 }
